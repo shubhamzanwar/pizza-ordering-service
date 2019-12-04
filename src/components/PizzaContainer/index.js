15c28b91c5695124442c2ebefe5416fa51eef07f
ShubhamZanwar <zanwar.shubham@gmail.com>
2019-12-04 23:10:51 +0530

    components: adding the pizza container component

---
+++ src/components/PizzaContainer/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Pizza from './Pizza';
+import ToppingsPallette from './ToppingsPallette';
+import * as styles from './index.module.css';
+
+const PizzaContainer = ({ toppings, index }) => (
+  <div className={styles['pizza-container']}>
+    <Pizza toppings={toppings} />
+    <ToppingsPallette toppings={toppings} index={index} />
+  </div>
+);
+
+PizzaContainer.propTypes = {
+  toppings: PropTypes.shape({
+    greenPepper: PropTypes.bool,
+    redPepper: PropTypes.bool,
+    olives: PropTypes.bool,
+    sausages: PropTypes.bool,
+    tomato: PropTypes.bool,
+    mushrooms: PropTypes.bool,
+  }),
+  index: PropTypes.number,
+};
+
+PizzaContainer.defaultProps = {
+  toppings: {
+    greenPepper: false,
+    redPepper: false,
+    olives: false,
+    sausages: false,
+    tomato: false,
+    mushrooms: false,
+  },
+  index: 0,
+};
+
+export default PizzaContainer;
