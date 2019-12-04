4b13afcaa520156290a305fc7a1089bba93cd637
ShubhamZanwar <zanwar.shubham@gmail.com>
2019-12-04 19:16:51 +0530

    app: setting up the router and navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import * as styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.app}>
+      <header className={styles['app-header']}>
+        <Link className={styles['header-button']} to="/">Home</Link>
+        <Link className={styles['header-button']} to="/checkout">Checkout</Link>
       </header>
+      <div className={styles.content}>
+        <Switch>
+          <Route exact path="/" render={() => <div>home</div> }/>
+          <Route path="/checkout" render={() => <div>Checkout</div> }/>
+        </Switch>
+      </div>
     </div>
   );
 }
