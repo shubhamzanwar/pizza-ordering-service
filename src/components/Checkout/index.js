3e883a81cd7385d7fdeb952034ba791633d03971
ShubhamZanwar <zanwar.shubham@gmail.com>
2019-12-06 18:48:37 +0530

    components: adding the receipt component in the checkout page

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CheckoutSummary from '../CheckoutSummary';
+import Receipt from '../Receipt';
 import * as styles from './index.module.css';
 
 const Checkout = () => (
   <div className={styles['checkout-container']}>
     <CheckoutSummary />
+    <Receipt />
   </div>
 );
 
