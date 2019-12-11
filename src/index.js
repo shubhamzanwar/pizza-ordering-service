468fac756d51bd7187e5dd6b22ee120b3dfeb540
ShubhamZanwar <zanwar.shubham@gmail.com>
2019-12-11 22:33:18 +0530

    store: creating our own store, context and hoc

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from './redux/store';
 import App from './App';
-import store from './redux/store';
+
 import './index.css';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,32 @@
-import { createStore } from 'redux';
-import reducer from './reducers';
+// import { createStore } from 'redux';
+import React from 'react';
+import PropTypes from 'prop-types';
+import reducer, { initialState } from './reducers';
 
-const store = createStore(reducer);
-export default store;
+// const store = createStore(reducer);
+// export default store;
+export const StoreContext = React.createContext();
+
+export const Provider = ({ children }) => {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  return (
+    <StoreContext.Provider value={{ state, dispatch }}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
+
+Provider.propTypes = {
+  children: PropTypes.node,
+};
+
+Provider.defaultProps = {
+  children: null,
+};
+
+export const withGlobalStore = (Component) => (props) => {
+  const { state, dispatch } = React.useContext(StoreContext);
+  return (
+    <Component state={state} dispatch={dispatch} {...props} />
+  );
+};
