6cce6ee78b08a7e0cfeca91aa5e47d011b394fb1
ShubhamZanwar <zanwar.shubham@gmail.com>
2019-12-11 22:33:54 +0530

    reducer: refactoring reducer to use immer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,42 +1,26 @@
+import produce from 'immer';
 import { TOGGLE_TOPPING, ADD_NEW_PIZZA, DELETE_PIZZA } from '../actions';
 import { defaultToppings } from '../../constants/pizza';
 
-const initialState = {
+export const initialState = {
   pizzas: [{ ...defaultToppings }],
 };
 
-export default (state = initialState, action) => {
+export default produce((state = initialState, action) => {
   switch (action.type) {
     case TOGGLE_TOPPING: {
-      const { pizzas } = state;
       const { index, topping } = action.payload;
-      pizzas[index] = {
-        ...pizzas[index],
-        [topping]: !pizzas[index][topping],
-      };
-      return {
-        ...state,
-        pizzas: [...pizzas],
-      };
+      state.pizzas[index][topping] = !state.pizzas[index][topping];
+      break;
     }
     case ADD_NEW_PIZZA:
-      return {
-        ...state,
-        pizzas: [
-          ...state.pizzas,
-          { ...defaultToppings },
-        ],
-      };
+      state.pizzas.push({ ...defaultToppings });
+      break;
     case DELETE_PIZZA: {
       const { index } = action.payload;
-      const { pizzas } = state;
-      pizzas.splice(index, 1);
-      return {
-        ...state,
-        pizzas: [...pizzas],
-      };
+      state.pizzas.splice(index, 1);
+      break;
     }
     default:
-      return state;
   }
-};
+});
